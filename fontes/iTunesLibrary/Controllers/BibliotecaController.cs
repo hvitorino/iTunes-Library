d35cc9c69122d8bbb89855a8bedf47b533aff79a
Hamon Vitorino <hamon.vitorino@gmail.com>
2011-04-10 11:31:32 -0300

    Novos controllers adicionados

---
+++ fontes/iTunesLibrary/Controllers/BibliotecaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iTunesLibrary.Controllers
+{
+    public class BibliotecaController : Controller
+    {
+        //
+        // GET: /Biblioteca/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ fontes/iTunesLibrary/Controllers/MusicaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iTunesLibrary.Controllers
+{
+    public class MusicaController : Controller
+    {
+        //
+        // GET: /Musica/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
