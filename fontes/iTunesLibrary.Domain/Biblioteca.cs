3bbb02bed3002e8fcdef27d288d4f628399d77eb
Hamon Vitorino <hamon.vitorino@gmail.com>
2011-04-10 10:33:13 -0300

    Novas classes adicionadas

---
+++ fontes/iTunesLibrary.Domain/Biblioteca.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iTunesLibrary.Domain
+{
+	public class Biblioteca
+	{
+		public Biblioteca()
+		{
+			Id = Guid.NewGuid();
+		}
+
+		public Guid Id { get; set; }
+		public string Nome { get; set; }
+		public Usuario Autor { get; set; }
+		public IList<Musica> Musicas { get; set; }
+
+		public void adicionaMusica( Musica musica )
+		{
+			if ( Musicas == null )
+				Musicas = new List<Musica>() { musica };
+		}
+	}
+}
---
+++ fontes/iTunesLibrary.Domain/Musica.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iTunesLibrary.Domain
+{
+	public class Musica
+	{
+		public Musica()
+		{
+			Id = Guid.NewGuid();
+		}
+
+		public Guid Id { get; set; }
+		public string Nome { get; set; }
+		public string Artista { get; set; }
+
+		public Musica ComNome( string nome )
+		{
+			Nome = nome;
+
+			return this;
+		}
+
+		public Musica DoArtista( string artista )
+		{
+			Artista = artista;
+
+			return this;
+		}
+	}
+}
