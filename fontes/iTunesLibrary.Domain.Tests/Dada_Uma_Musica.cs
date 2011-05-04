9ac4f5b8ebc2ddca32af0119d874bda77fbfdfc4
Hamon Vitorino <hamon.vitorino@gmail.com>
2011-05-04 14:29:20 -0300

    Teste unitario para Musica

---
+++ fontes/iTunesLibrary.Domain.Tests/Dada_Uma_Musica.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using iTunesLibrary.Domain;
+
+namespace iTunesLibrary.Domain.Tests
+{
+	[TestFixture]
+	public class Dada_Uma_Musica
+	{
+		[TestCase]
+		public void Posso_Definir_Seu_Nome()
+		{
+			var musica = new Musica();
+
+			musica.ComNome("Smells Like Teen Spirit");
+
+			Assert.AreEqual("Smells Like Teen Spirit", musica.Nome);
+		}
+
+		[TestCase]
+		public void Posso_Definir_Nome_Do_Artista()
+		{
+			var musica = new Musica();
+
+			musica.DoArtista("Nirvana");
+
+			Assert.AreEqual("Nirvana", musica.Artista);
+		}
+	}
+}
